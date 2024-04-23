fb9090eab2673945ed3ac45d722960c3988b8f39
2rage <the2rage@icloud.com>
2024-04-23 21:37:59 +0300

    Update and fix bug with dependencies lead/deal

--- tgbot/tgbot.py
+++ tgbot/tgbot.py
@@ -4,35 +4,59 @@ import requests
 
 TOKEN = 'REMOVED'
 BITRIX_WEBHOOK = 'REMOVED'
-BITRIX_WEBOOK2 = 'https://b24-355fx6.bitrix24.ru/rest/1/45l37h83ivtpiqsl/'
+BITRIX_WEBOOK2 = 'REMOVED'
+#REMOVEDcrm.lead.fields.json
+
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Здравствуйте, напишите ваш вопрос")
 
 def handle_message(update: Update, context: CallbackContext):
     text = update.message.text
-    response = requests.post(f"{BITRIX_WEBHOOK}/crm.lead.add", json={'fields': {'TITLE': 'Новый запрос', 'COMMENTS': text}})
+    response = requests.post(f"{BITRIX_WEBHOOK}/crm.lead.add", json={'fields': {'TITLE': 'Новый лид из Telegram', 'COMMENTS': text}})
     lead_id = response.json()['result']
+    context.user_data['current_lead_id'] = lead_id 
     update.message.reply_text("Пожалуйста, укажите ваше ФИО, телефон и почту. Ответ отправьте в формате: ФИО, Телефон, Почта.")
+
 
 def handle_form(update: Update, context: CallbackContext):
     user_data = update.message.text.split(", ")
     if len(user_data) == 3:
         fio, phone, email = user_data
-        lead_id = context.user_data['current_lead_id'] 
-        requests.post(f"{BITRIX_WEBOOK2}/crm.lead.update", json={
-            'id': lead_id,
-            'fields': {'NAME': fio, 'PHONE': [{'VALUE': phone}], 'EMAIL': [{'VALUE': email}], 'STATUS_ID': '2'}
-        })
-        update.message.reply_text(f"{fio} спасибо, мы получили ваши данные, менеджер вам скоро напишет.")
+        lead_id = context.user_data.get('current_lead_id')
+        if lead_id:
 
-        requests.post(f"{BITRIX_WEBHOOK}/tasks.task.add", json={
-            'fields': {
-                'TITLE': 'Новый клиент для связи',
-                'DESCRIPTION': f'Свяжитесь с клиентом {fio}. Телефон: {phone}, Email: {email}',
-                'RESPONSIBLE_ID': 1  # ID ответственного сотрудника
-            }
-        })
+            names = fio.split()
+            first_name = names[0] if len(names) > 0 else ''
+            last_name = names[1] if len(names) > 1 else ''
+            second_name = names[2] if len(names) > 2 else ''
+            
+            lead_update_response = requests.post(f"{BITRIX_WEBOOK2}/crm.lead.update", json={
+                'id': lead_id,
+                'fields': {
+                    'NAME': first_name,
+                    'LAST_NAME': last_name,
+                    'SECOND_NAME': second_name,
+                    'PHONE': [{'VALUE': phone, 'VALUE_TYPE': 'WORK'}],
+                    'EMAIL': [{'VALUE': email, 'VALUE_TYPE': 'WORK'}],
+                    'STATUS_ID': '2'
+                }
+            })
+            if lead_update_response.status_code == 200:
+                update.message.reply_text(f"{fio} спасибо, мы получили ваши данные, менеджер вам скоро напишет.")
+
+                requests.post(f"{BITRIX_WEBOOK2}/tasks.task.add", json={
+                    'fields': {
+                        'TITLE': 'Новый клиент для связи',
+                        'DESCRIPTION': f'Свяжитесь с клиентом {fio}. Телефон: {phone}, Email: {email}',
+                        'RESPONSIBLE_ID': 1  # ID ответственного сотрудника
+                    }
+                })
+            else:
+                update.message.reply_text("Не удалось обновить данные лида. Пожалуйста, попробуйте еще раз.")
+        else:
+            update.message.reply_text("Произошла ошибка при обработке вашего запроса, пожалуйста, начните заново.")
+
 
 def main():
     updater = Updater(TOKEN, use_context=True)
