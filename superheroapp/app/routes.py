48c27c819bfb35adbfe6e4e45c3e299d5c63705a
2rage <the2rage@icloud.com>
2024-04-25 14:46:51 +0300

    Update superhero app. DB migrate to SQL Alchemy. App is running from one file.

---
+++ superheroapp/app/routes.py
@@ -0,0 +1,63 @@
+from flask import render_template
+from .models import Superhero, Appearance
+from .database import db
+from .models import Superhero
+
+def create_routes(app):
+
+    @app.route('/top5_by_strength')
+    def top5_by_strength():
+        # Фильтрация записей, где 'strength' не является ни NULL, ни пустой строкой
+        heroes = Superhero.query.filter(Superhero.strength != 'null', Superhero.strength.isnot(None)).order_by(Superhero.strength.desc()).limit(5).all()
+
+        headers = ["Name", "Strength"]
+        data = [
+            [hero.name, hero.strength] for hero in heroes if hero.strength
+        ]
+
+        return render_template('table.html', title="Топ 5 супергероев по силе", headers=headers, data=data)
+
+
+    @app.route('/heroes_tall_strong')
+    def heroes_tall_strong():
+        heroes = db.session.query(Superhero).join(Appearance).filter(
+            Appearance.height > '180', Superhero.strength > 80
+        ).all()
+        
+        headers = ["Имя", "Интеллект", "Сила", "Скорость", "Мощность", "Пол", "Раса", "Рост", "Вес"]
+
+        data = [
+            [
+                hero.name, hero.intelligence, hero.strength, hero.speed, hero.power,
+                hero.appearances[0].gender, hero.appearances[0].race,
+                hero.appearances[0].height, hero.appearances[0].weight
+            ] for hero in heroes
+        ]
+
+        return render_template('table.html', title="Герои выше 180 см и с силой более 80", headers=headers, data=data)
+
+
+    @app.route('/average_by_gender')
+    def average_by_gender():
+        data = db.session.query(
+            Appearance.gender,
+            db.func.avg(Superhero.intelligence).label('average_intelligence'),
+            db.func.avg(Superhero.strength).label('average_strength'),
+            db.func.avg(Superhero.speed).label('average_speed'),
+            db.func.avg(Superhero.power).label('average_power')
+        ).join(Superhero).group_by(Appearance.gender).all()
+
+        headers = ["Пол", "Средний интеллект", "Средняя сила", "Средняя скорость", "Средняя мощность"]
+
+        rows = [[d.gender, round(d.average_intelligence, 2), round(d.average_strength, 2),
+                round(d.average_speed, 2), round(d.average_power, 2)] for d in data]
+
+        return render_template('table.html', title="Средние значения по полу", headers=headers, data=rows)
+
+    
+    @app.route('/')
+    def index():
+        heroes = Superhero.query.all()  # Загрузка всех супергероев из базы данных
+        return render_template('index.html', heroes=heroes)
+
+    return app
